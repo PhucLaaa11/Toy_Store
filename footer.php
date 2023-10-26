fcdb3f3114965c1f01dd1a550a7ec55bfa4894b2
PhucLaaa11 <phuclhgcc210353@fpt.edu.vn>
2023-10-26 08:40:45 +0700

    test5

--- footer.php
+++ footer.php
@@ -1,15 +1,12 @@
 <!--Footer-->
+<div class="b-example-divider"></div>
 <footer class="text-muted py-5">
     <div class="container">
-        <p class="float-end mb-1">
-            <a href="#" class="text-decoration-none">Back to top</a>
-        </p>
-        <p>
-            &copy; 2017-2022 Company, Inc. &middot;
-            <a href="#" class="text-decoration-none">Privacy</a> &middot;
-            <a href="#" class="text-decoration-none">Terms</a>>
-        </p>
+        <h2>Store</h2>
+        <p>33-35 D4, Tan Hung, 7 District, HCM</p>
+        <p>30/4 Street, Ninh Kieu District, Can Tho</p>
+        <p>206G Doi Can, Ba Dinh District, Ha Noi</p>
+        <p>&copy; 2017-2022 company, Inc.Terms Privacy&middot;</p>
+        <p class="float-end mb-1"><a href="#">Back to top</a></p>
     </div>
 </footer>
-
-</html>
