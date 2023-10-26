63e9dcc2006b0ea12de0d0deec0016ca7ba56fe6
PhucLaaa11 <phuclhgcc210353@fpt.edu.vn>
2023-10-26 09:28:50 +0700

    test10

--- connect.php
+++ connect.php
@@ -5,10 +5,10 @@ class Connect{
     public $username;
     public $password;
     public function __construct(){
-        $this->server = "localhost";
-        $this->username = "root";
-        $this->password = "";
-        $this->dbName= "shop_210353";
+        $this->server = "lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $this->username = "tnif9npq85u2l1fx";
+        $this->password = "ni1o8oho32b5hp81";
+        $this->dbName= "oe724m7i3qgzsikf";
     }
 
     // Option 1: mysqli
